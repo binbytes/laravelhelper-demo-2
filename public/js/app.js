bc4093a762b699681bd9f328dbd075d039a4ac1c
Dhara Ranipa <dhara.binbytes@gmail.com>
2019-06-03 17:35:24 +0530

    -add relationship in datatable

--- public/js/app.js
+++ public/js/app.js
@@ -66955,8 +66955,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/binbytes-4/Sites/generator2/resources/assets/js/app.js */"./resources/assets/js/app.js");
-module.exports = __webpack_require__(/*! /Users/binbytes-4/Sites/generator2/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
+__webpack_require__(/*! /Users/binbytes-4/Sites/laravelhelper-demo-2/resources/assets/js/app.js */"./resources/assets/js/app.js");
+module.exports = __webpack_require__(/*! /Users/binbytes-4/Sites/laravelhelper-demo-2/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
 
 
 /***/ })
